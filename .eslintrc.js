4fc8d96363be8adc05172d097c3248c918f442a4
Pong <samfunghp@gmail.com>
2022-12-15 18:04:26 +0800

    feat(): add husky, lint-staged, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'react-hooks', 'jest'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'react-app/jest'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  rules: {
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true, argsIgnorePattern: '_' }],
+    '@typescript-eslint/no-namespace': [2, { allowDeclarations: true }],
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: []
+};
