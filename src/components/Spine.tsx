5891df340316958a32dde0c9a4b3a54108579363
Pong <samfunghp@gmail.com>
2022-12-15 17:52:50 +0800

    feat(): add spine

---
+++ src/components/Spine.tsx
@@ -0,0 +1,15 @@
+import { forwardRef, Ref, useImperativeHandle } from 'react';
+import { SpinePlayer, useSpine, UseSpineOption } from '@/hooks/useSpine';
+
+export interface SpineComponentProps extends UseSpineOption {}
+
+function SpineComponent(
+  { jsonUrl, atlasUrl, animation, config }: SpineComponentProps,
+  ref: Ref<SpinePlayer | undefined>
+) {
+  const [nodeRef, spine] = useSpine({ jsonUrl, atlasUrl, animation, config });
+  useImperativeHandle(ref, () => spine, [spine]);
+  return <div ref={nodeRef} />;
+}
+
+export const Spine = forwardRef(SpineComponent);
