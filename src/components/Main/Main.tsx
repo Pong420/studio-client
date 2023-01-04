5cccffa355fd8b819da776a0278a6552b00ec0a4
sam.fung <sam.fung@teamcreation.co.uk>
2023-01-04 21:05:35 +0800

    feat(): add controller props to main container

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,26 +7,38 @@ export interface MainProps {
   bottom?: React.ReactNode;
   aligment?: boolean;
   background?: boolean;
+  buttons?: (React.ComponentProps<'button'> & { text: string })[];
 }
 
 const sectorStyles: React.CSSProperties = {
   clipPath: `polygon(${getSectorPoints(75)})`
 };
 
-export function Main({ top, bottom, aligment, background }: MainProps) {
+export function Main({ top, bottom, aligment, background, buttons }: MainProps) {
   return (
-    <div
-      className={[classes.root, aligment && classes.aligment, background && classes.background]
-        .filter(Boolean)
-        .join(' ')
-        .trim()}
-    >
-      <div className={classes.sector} style={sectorStyles}>
-        <div className={classes.ring} />
-        <div className={classes.top}>{top}</div>
-        <div className={classes.blocker} />
+    <div className={classes.root}>
+      <div
+        className={[classes.main, aligment && classes.aligment, background && classes.background]
+          .filter(Boolean)
+          .join(' ')
+          .trim()}
+      >
+        <div className={classes.sector} style={sectorStyles}>
+          <div className={classes.ring} />
+          <div className={classes.top}>{top}</div>
+          <div className={classes.blocker} />
+        </div>
+        <div className={classes.bottom}>{bottom}</div>
       </div>
-      <div className={classes.bottom}>{bottom}</div>
+      {process.env.NODE_ENV === 'development' && (
+        <div className={classes.buttons}>
+          {buttons?.map(({ text, ...props }) => (
+            <button key={text} {...props}>
+              {text}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
