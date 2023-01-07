b05c1d0efac71fcca8d4c6c995451454cbbbd70a
sam.fung <sam.fung@teamcreation.co.uk>
2023-01-08 00:01:20 +0800

    refactor(): make it better

--- src/games/Entry.tsx
+++ src/games/Entry.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from '@/routes';
 
-const getComponentName = (element: ReactNode | null) => {
+export const getComponentName = (element: ReactNode | null) => {
   if (element && typeof element === 'object' && 'type' in element) {
     if (element['type'] instanceof Function) {
       return element['type']['name'];
@@ -13,7 +13,7 @@ const getComponentName = (element: ReactNode | null) => {
 export function Entry() {
   return (
     <ul>
-      {routes.map(r =>
+      {routes[0].children?.map(r =>
         r.path === '/' ? null : (
           <li key={r.path}>
             <Link to={r.path || '/'}>{getComponentName(r.element) || r.path}</Link>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,23 +1,42 @@
-import { RouteObject, createHashRouter } from 'react-router-dom';
-import { Entry } from './games/Entry';
+import { RouteObject, createHashRouter, useOutlet } from 'react-router-dom';
+import { Entry, getComponentName } from './games/Entry';
 import { BonusPick } from './games/BonusPick';
 import { LuckyCoinFlip } from './games/LuckyCoinFlip';
+import { useEffect } from 'react';
+
+function Layout() {
+  const outlet = useOutlet();
+
+  useEffect(() => {
+    const title = getComponentName(outlet?.props?.children?.props?.children)
+      ?.replace(/[A-Z]/g, s => ' ' + s)
+      .trim();
+    document.title = title || `Bonus Game`;
+  }, [outlet]);
+
+  return outlet;
+}
 
 /**
  * Note, you must add `as const` to the path attribute. Otherwise, the type checking will be broken.
  */
 export const routes: RouteObject[] = [
   {
-    path: '/',
-    element: <Entry />
-  },
-  {
-    path: '/bonus-pick',
-    element: <BonusPick />
-  },
-  {
-    path: '/lucky-coin-clip',
-    element: <LuckyCoinFlip />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Entry />
+      },
+      {
+        path: '/bonus-pick',
+        element: <BonusPick />
+      },
+      {
+        path: '/lucky-coin-clip',
+        element: <LuckyCoinFlip />
+      }
+    ]
   }
 ];
 
