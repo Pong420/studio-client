e6e188a56dc67b329a623e9855457fd97258a6d9
sam.fung <sam.fung@teamcreation.co.uk>
2023-01-08 13:18:41 +0800

    refactor(): lucky coin

--- src/games/LuckyCoinFlip/LuckyCoinFlip.tsx
+++ src/games/LuckyCoinFlip/LuckyCoinFlip.tsx
@@ -2,11 +2,52 @@ import { useState, useRef, ReactNode } from 'react';
 import { Main } from '@/components/Main';
 import { LuckyCoinMarquee, LuckyCoinMarqueeHandler } from './LuckyCoinMarquee';
 import { LuckyCoinCountDown } from './LuckyCoinCountDown';
-import { LuckyCoin } from './LuckyCoin/LuckyCoin';
+import { LuckyCoin, LuckyCoinProps } from './LuckyCoin/LuckyCoin';
 
 const assetsCtx = require.context('./assets', true, /(\.png|\.jpg)$/);
 
-const Coin = <LuckyCoin color="red" value="123" style={{ width: '15em' }} glow />;
+const coins: LuckyCoinProps[] = [
+  { color: 'red', value: '2' },
+  { color: 'red', value: '4' },
+  { color: 'red', value: '8' },
+  { color: 'red', value: '16' },
+  { color: 'red', value: '26' },
+  { color: 'red', value: '32' },
+  { color: 'red', value: '44' },
+  { color: 'red', value: '144' },
+  { color: 'red', value: '168' },
+  { color: 'red', value: '200' },
+  { color: 'blue', value: '3' },
+  { color: 'blue', value: '17' },
+  { color: 'blue', value: '25' },
+  { color: 'blue', value: '39' },
+  { color: 'blue', value: '41' },
+  { color: 'blue', value: '55' },
+  { color: 'blue', value: '79' },
+  { color: 'blue', value: '163' },
+  { color: 'blue', value: '175' },
+  { color: 'blue', value: '199' }
+];
+
+const Coin = (
+  <div
+    style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(5, auto)`,
+      alignItems: 'center',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      padding: `10%`,
+      gap: `10px`
+    }}
+  >
+    {coins.map((p, i) => (
+      <LuckyCoin key={i} {...p} />
+    ))}
+  </div>
+);
+
 const CountDown = <LuckyCoinCountDown />;
 
 export function LuckyCoinFlip() {
