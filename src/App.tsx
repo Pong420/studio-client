55f55c9741c046fc93413b19387f828f9e1298a4
sam.fung <sam.fung@teamcreation.co.uk>
2023-01-18 14:48:54 +0000

    fix(): remove preload assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
-import { usePreloadAssets } from './hooks/usePreloadAssets';
-
-const assetsCtx = require.context('./assets', true, /(\.png|\.jpg)$/);
 
 function App() {
-  const preload = usePreloadAssets(assetsCtx);
-
-  return preload.done ? <RouterProvider router={router} /> : null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
